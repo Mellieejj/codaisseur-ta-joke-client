da18b69d51c1deb2d71fc74b62ece3586541c27d
Mellieejj <Melanieverheul1992@gmail.com>
2020-04-23 22:06:23 +0200

    Add share social media share buttons

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { getJoke } from "../actions";
 import { connect } from "react-redux";
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon, RedditShareButton, RedditIcon, LinkedinShareButton, LinkedinIcon } from "react-share";
 
 class Jokes extends Component {
   state = {
@@ -18,18 +19,38 @@ class Jokes extends Component {
   }
 
   render() {
-    const {joke} = this.props
-    console.log("joke", this.props.joke);
+    const { joke } = this.props;
+    const toShareJoke = joke
+      ? joke[0].setup + " " + joke[0].punchline
+      : null;
+
+    const shareButtons = <div>
+      <FacebookShareButton children="Programming Jokes" url="https://www.haakdraak.nl" quote={toShareJoke}>
+    <FacebookIcon round="true" size="2.5rem"/>
+    </FacebookShareButton>
+    
+    <TwitterShareButton url="https://www.haakdraak.nl" children="Programming Jokes" title={toShareJoke}>
+    <TwitterIcon round="true" size="2.5rem" />
+    </TwitterShareButton>
+
+    <RedditShareButton url="https://www.haakdraak.nl" children="Programming Jokes" title={toShareJoke}>
+      <RedditIcon round="true" size="2.5rem" />
+    </RedditShareButton>
+
+    <LinkedinShareButton url="https://www.haakdraak.nl" children="Programming Jokes" summary={toShareJoke}>
+      <LinkedinIcon round="true" size="2.5rem" />
+    </LinkedinShareButton>
+    </div>
+
 
     return (
+      <>
       <div className="Joke">
-        <p>
-          {joke
-            ? joke[0].setup
-            : "Loading a joke for you.... "}
-        </p>
-        <p>{this.state.timeout ?  joke[0].punchline : null}</p>
+        <p>{joke ? joke[0].setup : "Loading a joke for you.... "}</p>
+        <p>{this.state.timeout ? joke[0].punchline : null}</p>
       </div>
+      {shareButtons}
+    </>
     );
   }
 }
