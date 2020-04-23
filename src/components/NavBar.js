d76aa744268905d257e78d8bc0cb4740378ceeec
Mellieejj <Melanieverheul1992@gmail.com>
2020-04-23 16:02:30 +0200

    made redux store

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const NavBar = () => {
+ return (
+   <div className="navbar">
+     <div className="title">Programming Jokes</div>
+     {/* <div className="auth">Login</div>
+     <div className="auth">Sign-up</div> */}
+  </div>
+ )
+}
+
+export default NavBar
